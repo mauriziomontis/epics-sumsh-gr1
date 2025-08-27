f17315e9ff34048385916e618c93ab2a8804dcb0
Marco Andres Auza Sanchez <aauzasanchez@tudelft.nl>
2025-08-27 14:44:56 +0200

    Update test_pwrspl.py

--- Blue_Sky/test_pwrspl.py
+++ Blue_Sky/test_pwrspl.py
@@ -1,18 +1,28 @@
 #!/usr/bin/env python3
 
 from bluesky import RunEngine
-from bluesky.plans import count, scan, list_scan
+from bluesky.plans import scan
 from bluesky.callbacks import LiveTable
-from PSU_ophyd import pwrspl
-
+from blue_pwrspl import pwrspl
 
 def callback(status):
-	print("Done: ", status)
-	
-psu = pwrspl("G1:Power:", name="psu")
-status = psu.set(4)
-print(status)
+    print("Done:", status)
 
+# Instantiate with the new PV prefix
+psu = pwrspl("", name="psu")
+
+# Turn on and set to 4 A
+status = psu.set(4)
+status.add_callback(callback)
+status.wait()  # block until complete
+
+# Create and configure the RunEngine
 RE = RunEngine()
-token = RE.subscribe(LiveTable([psu.current_RB]))
-RE(scan([], psu, 0, 5, 6))
+
+# Subscribe a LiveTable to show the current readback
+RE.subscribe(LiveTable([psu.current_RB]))
+
+# Perform a scan moving the PSU setpoint from 0 A to 7 A in 11 steps
+RE(scan([], psu, -7, 7, 15))
+
+psu.toff()
